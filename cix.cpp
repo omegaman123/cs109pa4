0ed73eaf6427e2fa366f422554a4239982620a2a
Mark <markmdvd@gmail.com>
2019-05-25 19:06:54 -0700

    Skelton for client.

--- cix.cpp
+++ cix.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <regex>
+
+
 using namespace std;
 
 #include <libgen.h>
@@ -15,13 +18,17 @@ using namespace std;
 #include "logstream.h"
 #include "sockets.h"
 
-logstream outlog (cout);
-struct cix_exit: public exception {};
+logstream outlog(cout);
+struct cix_exit : public exception {
+};
 
-unordered_map<string,cix_command> command_map {
-   {"exit", cix_command::EXIT},
-   {"help", cix_command::HELP},
-   {"ls"  , cix_command::LS  },
+unordered_map <string, cix_command> command_map{
+        {"exit", cix_command::EXIT},
+        {"help", cix_command::HELP},
+        {"ls",   cix_command::LS},
+        {"get",  cix_command::GET},
+        {"put",  cix_command::PUT},
+        {"rm",   cix_command::RM}
 };
 
 static const char help[] = R"||(
@@ -34,75 +41,130 @@ rm filename  - Remove file from remote server.
 )||";
 
 void cix_help() {
-   cout << help;
+    cout << help;
 }
 
-void cix_ls (client_socket& server) {
-   cix_header header;
-   header.command = cix_command::LS;
-   outlog << "sending header " << header << endl;
-   send_packet (server, &header, sizeof header);
-   recv_packet (server, &header, sizeof header);
-   outlog << "received header " << header << endl;
-   if (header.command != cix_command::LSOUT) {
-      outlog << "sent LS, server did not return LSOUT" << endl;
-      outlog << "server returned " << header << endl;
-   }else {
-      auto buffer = make_unique<char[]> (header.nbytes + 1);
-      recv_packet (server, buffer.get(), header.nbytes);
-      outlog << "received " << header.nbytes << " bytes" << endl;
-      buffer[header.nbytes] = '\0';
-      cout << buffer.get();
-   }
+void cix_ls(client_socket &server) {
+    cix_header header;
+    header.command = cix_command::LS;
+    outlog << "sending header " << header << endl;
+    send_packet(server, &header, sizeof header);
+    recv_packet(server, &header, sizeof header);
+    outlog << "received header " << header << endl;
+    if (header.command != cix_command::LSOUT) {
+        outlog << "sent LS, server did not return LSOUT" << endl;
+        outlog << "server returned " << header << endl;
+    } else {
+        auto buffer = make_unique<char[]>(header.nbytes + 1);
+        recv_packet(server, buffer.get(), header.nbytes);
+        outlog << "received " << header.nbytes << " bytes" << endl;
+        buffer[header.nbytes] = '\0';
+        cout << buffer.get();
+    }
 }
 
-
+void cix_get(client_socket &server, const string &filename){
+ outlog << "cix_get " << filename << endl;
+    cix_header header;
+    header.command = cix_command::GET;
+    outlog << "sending header " << header << endl;
+    send_packet(server, &header, sizeof header);
+
+
+}
+
+void cix_rm(client_socket &server, const string &filename){
+  outlog << "cix_rm " << filename << endl;
+    cix_header header;
+    header.command = cix_command::RM;
+    outlog << "sending header " << header << endl;
+    send_packet(server, &header, sizeof header);
+
+}
+
+void cix_put(client_socket &server, const string &filename){
+    outlog << "cix_put " << filename << endl;
+    cix_header header;
+    header.command = cix_command::PUT;
+    outlog << "sending header " << header << endl;
+    send_packet(server, &header, sizeof header);
+    
+}
+
+
+
 void usage() {
-   cerr << "Usage: " << outlog.execname() << " [host] [port]" << endl;
-   throw cix_exit();
+    cerr << "Usage: " << outlog.execname() << " [host] [port]"
+         << endl;
+    throw cix_exit();
 }
 
-int main (int argc, char** argv) {
-   outlog.execname (basename (argv[0]));
-   outlog << "starting" << endl;
-   vector<string> args (&argv[1], &argv[argc]);
-   if (args.size() > 2) usage();
-   string host = get_cix_server_host (args, 0);
-   in_port_t port = get_cix_server_port (args, 1);
-   outlog << to_string (hostinfo()) << endl;
-   try {
-      outlog << "connecting to " << host << " port " << port << endl;
-      client_socket server (host, port);
-      outlog << "connected to " << to_string (server) << endl;
-      for (;;) {
-         string line;
-         getline (cin, line);
-         if (cin.eof()) throw cix_exit();
-         outlog << "command " << line << endl;
-         const auto& itor = command_map.find (line);
-         cix_command cmd = itor == command_map.end()
-                         ? cix_command::ERROR : itor->second;
-         switch (cmd) {
-            case cix_command::EXIT:
-               throw cix_exit();
-               break;
-            case cix_command::HELP:
-               cix_help();
-               break;
-            case cix_command::LS:
-               cix_ls (server);
-               break;
-            default:
-               outlog << line << ": invalid command" << endl;
-               break;
-         }
-      }
-   }catch (socket_error& error) {
-      outlog << error.what() << endl;
-   }catch (cix_exit& error) {
-      outlog << "caught cix_exit" << endl;
-   }
-   outlog << "finishing" << endl;
-   return 0;
+int main(int argc, char **argv) {
+    regex command{R"(^\s*[a-zA-Z]+\s+[^\/\s]+\s*$)"};
+    regex cmd_regex{R"(^\s*([a-z]+?)(\s+([^\/\s]{1,59}?))?\s*$)"};
+
+
+    outlog.execname(basename(argv[0]));
+    outlog << "starting" << endl;
+    vector <string> args(&argv[1], &argv[argc]);
+    if (args.size() > 2) usage();
+    string host = get_cix_server_host(args, 0);
+    in_port_t port = get_cix_server_port(args, 1);
+    outlog << to_string(hostinfo()) << endl;
+    try {
+        outlog << "connecting to " << host << " port " << port
+               << endl;
+        client_socket server(host, port);
+        outlog << "connected to " << to_string(server) << endl;
+        for (;;) {
+            string line;
+            getline(cin, line);
+            if (cin.eof()) throw cix_exit();
+
+            smatch result;
+            auto match = regex_search(line, result, cmd_regex);
+
+            if (!match) {
+                outlog << line << ": invalid command or filename"
+                       << endl;
+                continue;
+            }
+
+            outlog << "command " << line << endl;
+            const auto &itor = command_map.find(result[1]);
+            cix_command cmd = itor == command_map.end()
+                              ? cix_command::ERROR : itor->second;
+
+            switch (cmd) {
+                case cix_command::EXIT:
+                    throw cix_exit();
+                    break;
+                case cix_command::HELP:
+                    cix_help();
+                    break;
+                case cix_command::LS:
+                    cix_ls(server);
+                    break;
+                case cix_command::GET:
+                    cix_get(server, result[3]);
+                    break;
+                case cix_command::RM:
+                    cix_rm(server, result[3]);
+                    break;
+                case cix_command::PUT:
+                    cix_put(server, result[3]);
+                    break;
+                default:
+                    outlog << line << ": invalid command" << endl;
+                    break;
+            }
+        }
+    } catch (socket_error &error) {
+        outlog << error.what() << endl;
+    } catch (cix_exit &error) {
+        outlog << "caught cix_exit" << endl;
+    }
+    outlog << "finishing" << endl;
+    return 0;
 }
 
